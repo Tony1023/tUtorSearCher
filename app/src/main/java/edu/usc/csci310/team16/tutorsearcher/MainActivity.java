5c8000d76337689d2a267fbd7e4724f45a799309
Zhehao /Tony/ Lu <tony1023lv@gmail.com>
2019-10-30 10:50:24 -0700

    Changed Profile

---
+++ app/src/main/java/edu/usc/csci310/team16/tutorsearcher/MainActivity.java
@@ -0,0 +1,71 @@
+package edu.usc.csci310.team16.tutorsearcher;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentContainer;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+//public class MainActivity extends AppCompatActivity {
+//
+//    private MainModel mainModel;
+//    private FragmentContainer fragmentContainer;
+//    private ProfileFragment profile;
+//    private SearchActivity search;
+//    private NotificationActivity notification;
+//    private RatingActivity rating;
+//
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//
+//        profile = new ProfileFragment();
+//        search = new SearchActivity();
+//        notification = new NotificationActivity();
+//        rating = new RatingActivity();
+//
+//
+//        mainModel = new ViewModelProvider(this, new ViewModelProvider.NewInstanceFactory()).get(MainModel.class);
+//        Object menu = findViewById(R.id.navigation);
+//        menu.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                return onSelected(item);
+//            }
+//        });
+//
+//        getSupportFragmentManager().beginTransaction()
+//                .replace(R.id.fragment_container, profile)
+//                .commit();
+//    }
+//
+//    public boolean onSelected(@NonNull MenuItem item) {
+//        if (item.getItemId() == mainModel.getPage()) {
+//            return false;
+//        } else {
+//            Fragment view;
+//            switch (item.getItemId()) {
+//                case R.id.navigation_search:
+//                    view = search; break;
+//                case R.id.navigation_notifications:
+//                    view = notification; break;
+//                case R.id.navigation_tutors:
+//                    view = rating; break;
+//                case R.id.navigation_profile:
+//                default:
+//                    view = profile;
+//            }
+//            getSupportFragmentManager().beginTransaction()
+//                    .replace(R.id.fragment_container, view)
+//                    .commit();
+//            mainModel.setPage(item.getItemId());
+//            return true;
+//        }
+//    }
+//}
