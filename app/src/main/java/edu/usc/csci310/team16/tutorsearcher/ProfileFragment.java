349cd29debe728c5064582ab06dbf2dc5d585bb4
Teagan Ampe <ampe@usc.edu>
2019-11-02 17:54:51 -0700

    more working on profile layout

--- app/src/main/java/edu/usc/csci310/team16/tutorsearcher/ProfileFragment.java
+++ app/src/main/java/edu/usc/csci310/team16/tutorsearcher/ProfileFragment.java
@@ -30,8 +30,6 @@ public class ProfileFragment extends Fragment {
 
     }
 
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
