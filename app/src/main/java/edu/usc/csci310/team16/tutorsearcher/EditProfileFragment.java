068eeb67609121bf4d90cb61076984f920bde65f
Teagan Ampe <ampe@usc.edu>
2019-11-03 17:57:21 -0800

    working on edit profile page

--- app/src/main/java/edu/usc/csci310/team16/tutorsearcher/EditProfileFragment.java
+++ app/src/main/java/edu/usc/csci310/team16/tutorsearcher/EditProfileFragment.java
@@ -27,6 +27,17 @@ public class EditProfileFragment extends Fragment {
 
         View v = inflater.inflate(R.layout.edit_profile_fragment, container, false);
 
+        // Spinner element
+
+
+        /*
+            CS 103, CS 104, CS 170,
+            CS 270, CS 201, CS 310,
+            CS 356, CS 350, CS 360
+
+         */
+
+
         return v;
     }
 }
