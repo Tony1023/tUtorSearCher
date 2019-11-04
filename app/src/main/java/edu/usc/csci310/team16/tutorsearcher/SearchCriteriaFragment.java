9231ace24907e916dbbf84faf009a38a8ba47672
Micah Steinberg <mbsteinb@usc.edu>
2019-11-04 09:38:55 -0800

    Started buiilding time selector for search criteria form

--- app/src/main/java/edu/usc/csci310/team16/tutorsearcher/SearchCriteriaFragment.java
+++ app/src/main/java/edu/usc/csci310/team16/tutorsearcher/SearchCriteriaFragment.java
@@ -8,28 +8,32 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.GridLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
 
 public class SearchCriteriaFragment extends Fragment {
 
     private SearchModel searchModel;
-    private ArrayList<String> courses;
+    private ToggleButton time_toggle[][];
 
     @Override
     public void onCreate(Bundle savedBundleInstance) {
         super.onCreate(savedBundleInstance);
 
-//        searchModel = ViewModelProviders.of(getActivity()).get(SearchModel.class);
+        searchModel = ViewModelProviders.of(getActivity()).get(SearchModel.class);
+        time_toggle = new ToggleButton[searchModel.getDays().length][searchModel.getBlocks().length];
 
-        // TODO: get courses from call to db
-        courses = new ArrayList<String>(Arrays.asList("CSCI 103", "CSCI 104", "CSCI 201", "CSCI 310"));
+//        // TODO: get courses from call to db
+//        courses = new ArrayList<String>(Arrays.asList("CSCI 103", "CSCI 104", "CSCI 201", "CSCI 310"));
     }
 
 
@@ -39,9 +43,34 @@ public class SearchCriteriaFragment extends Fragment {
 
         Spinner courseSpinner = (Spinner) v.findViewById(R.id.course_spinner);
         final ArrayAdapter<String> courseSpinnerArrayAdapter = new ArrayAdapter<String>(
-                v.getContext(), R.layout.course_spinner_item, courses);
+                v.getContext(), R.layout.course_spinner_item, searchModel.getCourses());
         courseSpinnerArrayAdapter.setDropDownViewResource(R.layout.course_spinner_item);
         courseSpinner.setAdapter(courseSpinnerArrayAdapter);
+
+        GridLayout timeSelectGrid = (GridLayout) v.findViewById(R.id.time_select_grid);
+        timeSelectGrid.setColumnCount(time_toggle.length);
+        timeSelectGrid.setRowCount(1);
+        for(int i = 0; i < time_toggle.length; i++){
+            GridLayout g = new GridLayout(v.getContext());
+            g.setColumnCount(1);
+            g.setRowCount(time_toggle[0].length);
+
+            TextView t = new TextView(v.getContext());
+            t.setText(searchModel.getDays()[i]);
+            g.addView(t);
+
+            for(int j = 0; j < time_toggle[0].length; j++){
+                time_toggle[i][j] = new ToggleButton(v.getContext());
+                time_toggle[i][j].setText(searchModel.getBlocks()[j]);
+                time_toggle[i][j].setTextOn(searchModel.getBlocks()[j]);
+                time_toggle[i][j].setTextOff(searchModel.getBlocks()[j]);
+                g.addView(time_toggle[i][j]);
+            }
+
+            timeSelectGrid.addView(g);
+        }
+//        ToggleButton tb = new ToggleButton(v.getContext());
+//        timeSelectGrid.addView(tb);
 
         //when clicking search button, transition back to search page, with search results
         Button searchButton = (Button) v.findViewById(R.id.search_button);
--- app/src/main/java/edu/usc/csci310/team16/tutorsearcher/SearchModel.java
+++ app/src/main/java/edu/usc/csci310/team16/tutorsearcher/SearchModel.java
@@ -1,7 +1,52 @@
 package edu.usc.csci310.team16.tutorsearcher;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import androidx.lifecycle.ViewModel;
 
 public class SearchModel extends ViewModel {
+    private static final String[] days =
+            {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+    private static final String[] blocks =
+            {"8:00am", "8:30am", "9:00am", "9:30am", "10:00am", "10:30am", "11:00am",
+                    "11:30am", "12:00pm", "12:30pm", "1:00pm", "1:30pm", "2:00pm", "2:30pm",
+                    "3:00pm", "3:30pm", "4:00pm", "4:30pm", "5:00pm", "5:30pm", "6:00pm",
+                    "6:30pm", "7:00pm", "7:30pm", "8:00pm", "8:30pm", "9:00pm", "9:30pm"};
+    private static final String[] courses =
+            {"CSCI 103", "CSCI 104", "CSCI 170", "CSCI 201", "CSCI 270", "CSCI 310", "CSCI 350",
+                    "CSCI 356", "CSCI 360"};
 
+    private String course;
+    private List<Integer> availability;
+
+
+    public static String[] getDays() {
+        return days;
+    }
+
+    public static String[] getBlocks() {
+        return blocks;
+    }
+
+    public static String[] getCourses() {
+        return courses;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public List<Integer> getAvailability() {
+        return availability;
+    }
+
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public void setAvailability(List<Integer> availability) {
+        this.availability = availability;
+    }
 }
